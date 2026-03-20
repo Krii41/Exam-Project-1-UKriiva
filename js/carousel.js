e2412bb238f0dda72b9703a3c9b841b0bee7410a
Krii41 <134600504+Krii41@users.noreply.github.com>
2026-03-20 20:13:39 +0100

    Added JavaScript to carousel

--- js/carousel.js
+++ js/carousel.js
@@ -4,33 +4,59 @@ export function initCarousel() {
     document.querySelectorAll(".slider").forEach((slider) => {
         const items = slider.querySelectorAll(".slide");
         const dotContainer = slider.parentElement.querySelector(".dot-container");
-    
+
         const dotsHtml = Array.from(items, () => {
             return `<span class="dot"></span>`;
         }).join("");
-    
+
         dotContainer.innerHTML = dotsHtml;
-    
+
         const dots = dotContainer.querySelectorAll(".dot");
-    
+        let currentIndex = 0;
+        let intervalId;
+
+        function stopAutoplay() {
+            clearInterval(intervalId);
+        }
+
+        function startAutoplay() {
+            clearInterval(intervalId);
+            intervalId = setInterval(() => {
+                nextSlide();
+            }, 7000);
+        }
+
         function showSlide(index) {
             items.forEach((item) => item.classList.remove("selected"));
             dots.forEach((dot) => dot.classList.remove("dot-selected"));
-    
+
             items[index].classList.add("selected");
             dots[index].classList.add("dot-selected");
         }
-    
+
+        function nextSlide() {
+            currentIndex++;
+
+            if (currentIndex >= items.length) {
+                currentIndex = 0;
+            }
+
+            showSlide(currentIndex);
+        }
+
         dots.forEach((dot, i) => {
             dot.addEventListener("click", () => {
-                showSlide(i);
+                currentIndex = i;
+                showSlide(currentIndex);
             });
         });
-    
-        showSlide(0);
-    });
 
-    
-    
+        showSlide(currentIndex);
+
+        startAutoplay();
+
+        slider.addEventListener("mouseenter", stopAutoplay);
+        slider.addEventListener("mouseleave", startAutoplay);
+    });
 }
 
