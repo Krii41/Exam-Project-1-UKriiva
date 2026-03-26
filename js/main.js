ba9fba42b9324491691703e6aaea4008d81f92a0
Krii41 <134600504+Krii41@users.noreply.github.com>
2026-03-26 20:27:08 +0100

    Responsive cart

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 import { initCarousel } from "./carousel.js";
 
 async function loadComponent(id, file) {
+    const target = document.getElementById(id);
+    if (!target) return false;
+
     const response = await fetch(file);
     const html = await response.text();
-    document.getElementById(id).innerHTML = html;
+    target.innerHTML = html;
+    return true;
 }
 
 async function initPage() {
     await loadComponent("header", "../components/header.html");
     await loadComponent("footer", "../components/footer.html");
-    await loadComponent("hero-slider", "../components/carousel.html");
-    initCarousel();
+
+    const hasCarousel = await loadComponent(
+        "hero-slider",
+        "../components/carousel.html"
+    );
+
+    if (hasCarousel) {
+        initCarousel();
+    }
 }
 
 initPage();
