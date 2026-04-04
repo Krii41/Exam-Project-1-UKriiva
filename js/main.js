16c1dab43326069439ebf5b3c124dc55156a2726
Krii41 <134600504+Krii41@users.noreply.github.com>
2026-04-04 08:53:28 +0200

    JS test

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ async function initPage() {
 initPage();
  */
 
-import { initCarousel } from "./carousel.js";
+/* import { initCarousel } from "./carousel.js";
 
 const BASE_PATH = "/EXAM-PROJECT-1-UKRIIVA";
 
@@ -55,6 +55,45 @@ async function initPage() {
   const hasCarousel = await loadComponent(
     "hero-slider",
     "/components/carousel.html"
+  );
+
+  if (hasCarousel) {
+    initCarousel();
+  }
+}
+
+initPage(); */
+
+import { initCarousel } from "./carousel.js";
+
+async function loadComponent(id, relativePath) {
+  const target = document.getElementById(id);
+  if (!target) return false;
+
+  try {
+    const fileUrl = new URL(relativePath, import.meta.url);
+    const response = await fetch(fileUrl);
+
+    if (!response.ok) {
+      throw new Error(`Failed to load ${fileUrl} (${response.status})`);
+    }
+
+    const html = await response.text();
+    target.innerHTML = html;
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+async function initPage() {
+  await loadComponent("header", "../components/header.html");
+  await loadComponent("footer", "../components/footer.html");
+
+  const hasCarousel = await loadComponent(
+    "hero-slider",
+    "../components/carousel.html"
   );
 
   if (hasCarousel) {
